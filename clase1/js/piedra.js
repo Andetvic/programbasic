7522ee7b9ef60d2528a2e2350c5510d505f32857
t5t6pe <t5t6pe@gmail.com>
2014-08-12 23:01:15 -0500

    plop mas cambios
    pro compatibilidad

--- clase1/js/piedra.js
+++ clase1/js/piedra.js
@@ -34,24 +34,41 @@ function valor(ctrl){
 		
 		//Se crea la condicion "if" en caso que sea un empate
 		if (usuario === maquina){
-			document.getElementById("resultado").innerHTML = empate;
+			resultado = empate;
 		}
 		//Se crean las condiciones en las que el usuario gane y se muestran
 		else if( (opcionUsuario==0 && opcionMaquina==1) || (opcionUsuario==1 && opcionMaquina==2) || (opcionUsuario==2 && opcionMaquina==0)){
-			document.getElementById("resultado").innerHTML = ganaste;
+			resultado = ganaste;
 			puntajeUsuario = puntajeUsuario+1;
+			try{
 			console.log ("U:"+puntajeUsuario);
+			}catch(e){}
 		}
 		//ahora solo nos queda mostrar el resultado de perder
 		else {
-			document.getElementById("resultado").innerHTML = perdiste;
+			resultado = perdiste;
 			puntajeMaquina = puntajeMaquina+1;
+			try{
 			console.log ("M:"+puntajeMaquina);
+			}catch(e){}
+		}
+		
+		var ctrlResultado = document.getElementById("resultado");
+		try{
+			ctrlResultado.innerHTML = resultado;
+		}catch(e){
+			ctrlResultado.text = resultado;
 		}
 
 		//aqui vemos que recibimos despues del arreglo opciones
 		//console.log("Usuario y Maquina: " + usuario + " " +maquina);
 		//Se muestra al usuario el valor escogido y el valor que opcionMaquina escogio
 		var res = "<figure class='ScoreOption'>Jugador " + puntajeUsuario + " <br />" + usuario + "</figure><figure class='ScoreOption'>Maquina " + puntajeMaquina + " <br />" + maquina +"<figure>";
-		document.getElementById("desafio").innerHTML = res;	
+
+		var ctrlDesafio = document.getElementById("desafio");
+		try{
+			ctrlDesafio.innerHTML = res;
+		}catch(e){
+			ctrlDesafio.text = res;
+		}
 }
